8e39d7ea2e05449d263c87de09cf53bb850d0ed1
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-05-16 03:07:12 +0000

    Add Toaster component to Providers for enhanced notification support

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,20 @@
 "use client";
+import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "./theme-provider";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <Toaster />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
 export default Providers;
